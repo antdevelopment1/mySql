9f63a79aa0cebfb4bd705504b93acf96aee9e02a
April Copes <antdevelopment1@gmail.com>
2019-04-15 21:32:45 -0600

    adds comments for steps to run table in sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,11 @@
 --  Twitter Tweets Table
+-- mysql -u root -p password
+-- show databases
+-- use database
+-- paste create table statement into mysql
+
 CREATE TABLE tweets (
-    username varchar(16), 
+    username varchar(16),  
     tweet varchar(100), 
     likes INT
 );
