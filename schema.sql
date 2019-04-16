566da7ef1cf0d89e6517052b50fdf0b573a6d5af
April Copes <antdevelopment1@gmail.com>
2019-04-15 21:24:27 -0600

    Adds create table example making a database

---
+++ schema.sql
@@ -0,0 +1,11 @@
+--  Twitter Tweets Table
+CREATE TABLE tweets (
+    username varchar(16), 
+    tweet varchar(100), 
+    likes INT
+);
+
+CREATE TABLE cats (
+    name varchar(100),
+    age INT
+)
