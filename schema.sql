921291b204e16013608ecccab2423c5098b08274
April Copes <antdevelopment1@gmail.com>
2019-04-15 21:47:23 -0600

    Adds another example  for making a table in mysql

--- schema.sql
+++ schema.sql
@@ -4,13 +4,27 @@
 -- use database
 -- paste create table statement into mysql
 
-CREATE TABLE tweets (
-    username varchar(16),  
-    tweet varchar(100), 
-    likes INT
-);
+-- CREATE TABLE tweets (
+--     username varchar(16),  
+--     tweet varchar(100), 
+--     likes INT
+-- );
 
-CREATE TABLE cats (
-    name varchar(100),
-    age INT
-)
+-- CREATE TABLE cats (
+--     name varchar(100),
+--     age INT
+-- )
+
+-- Pastries Table
+-- At least two columns 
+-- Name for pastry and quantity 
+-- Name is 50 characters max
+-- Inspect the table/columns in the CLI
+-- Delete the table
+
+-- Create a bakery database
+CREATE TABLE pastries(
+    name varchar(50),
+    quantity INT,
+    delivery_date timestamp
+);
