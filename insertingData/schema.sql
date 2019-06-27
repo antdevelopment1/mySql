23dd7ed111985a64c358728e108baec288ee76cd
April Copes <antdevelopment1@gmail.com>
2019-06-27 16:42:07 -0400

    more pet data for crud operations in sql

--- insertingData/schema.sql
+++ insertingData/schema.sql
@@ -9,3 +9,24 @@ create table employees (
 
 insert into employees(first_name, last_name, age) values
 ("Dora", "Smith", 58);
+
+create table cats (
+	id int auto_increment not null primary key,
+	name varchar(20), 
+	breed varchar(20), 
+	age int
+);
+
+insert into cats
+	(name, breed, age) 
+values 
+	('Ringo', 'Tabby', 4), 
+	('Cindy', 'Maine Coon', 10), 
+	('Dumbledore', 'Miane Coon', 11), 
+	('Egg', 'Persian', 4), 
+	('Misty', 'Tabby', 13), 
+	('George Micheal', 'Ragdoll', 9), 
+	('Jackson', 'Sphynx', 7);
+
+
+	
