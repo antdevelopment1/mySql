4d1d9dcb2593f1e637c31f510170615e4467105f
April Copes <antdevelopment1@gmail.com>
2019-06-29 20:51:51 -0400

    changes unknown

--- insertingData/books.sql
+++ insertingData/books.sql
@@ -12,7 +12,7 @@ CREATE TABLE books
 
 INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages)
 VALUES
-('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291)`,
+('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
 ('Norse Mythology', 'Neil', 'Gaiman',2016, 43, 304),
 ('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
 ('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
