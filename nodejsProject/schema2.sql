152168d7ae4f190560a0ccae17c229363adc9212
April Copes <antdevelopment1@gmail.com>
2019-07-09 22:44:35 -0400

    finds earliest user my email

--- nodejsProject/schema2.sql
+++ nodejsProject/schema2.sql
@@ -1,3 +1,11 @@
--- Find the earliest date a user joined
+-- Challenge 1 sFind the earliest date a user joined
 select date_format(min(created_at), "%M %D %Y") as earliest_date 
 from users;
+
+-- Challenge 2 Find the email of the first user
+select email, min(created_at) from users
+    group by email
+    order by created_at limit 1;
+
+-- Challenge 2 Cleaner Solution
+select * from users where created_at = (select min(created_at) from users);
