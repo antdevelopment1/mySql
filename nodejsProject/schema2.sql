f1ea862404bda462d2714e678305d087dd59a0f1
April Copes <antdevelopment1@gmail.com>
2019-07-09 23:08:15 -0400

    finds all total users for each type of email provider

--- nodejsProject/schema2.sql
+++ nodejsProject/schema2.sql
@@ -17,3 +17,16 @@ select monthname(created_at) as month_joined, count(*) as count from users
 
 -- Challenge 4 Count the number of users with yahoo emails
 select count(*) from users where email like "%@yahoo.com%";
+
+-- Challenge 5 Calculate the total number of users for each email host
+select
+    case
+        when email like "%@yahoo.com" then "yahoo"
+        when email like "%@gmail.com" then "gmail"
+        when email like "%@hotmail.com" then "hotmail"
+        else 'other'
+    end as provider,
+    count(*) as total_users
+from users
+group by provider
+order by total_users desc;
