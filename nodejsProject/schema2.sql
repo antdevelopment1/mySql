d4f6c43325c256f16607f0583454e03cdd939139
April Copes <antdevelopment1@gmail.com>
2019-07-09 22:51:06 -0400

    Adds count of how many users joined in each month

--- nodejsProject/schema2.sql
+++ nodejsProject/schema2.sql
@@ -9,3 +9,8 @@ select email, min(created_at) from users
 
 -- Challenge 2 Cleaner Solution
 select * from users where created_at = (select min(created_at) from users);
+
+-- Challenge 3 Users according to month joined and count of users
+select monthname(created_at) as month_joined, count(*) as count from users
+    group by month_joined
+    order by count desc;
